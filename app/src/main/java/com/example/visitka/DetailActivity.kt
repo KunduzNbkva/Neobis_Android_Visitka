a689eaa12b30001b3551d8844f0199f53b0cbf43
kunduz <kunduznbkva@gmail.com>
2023-04-06 22:17:11 +0600

    customize appBarLayout handle click on email, map and phone

--- app/src/main/java/com/example/visitka/DetailActivity.kt
+++ app/src/main/java/com/example/visitka/DetailActivity.kt
@@ -1,9 +1,11 @@
 package com.example.visitka
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.example.visitka.databinding.ActivityDetailBinding
+
 
 class DetailActivity : AppCompatActivity() {
     lateinit var binding: ActivityDetailBinding
@@ -13,8 +15,30 @@ class DetailActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.tvDoctorsSurname.text = "Dr. ${intent.getStringExtra(MainActivity.SURNAME)}"
+
+        binding.callLayout.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_DIAL)
+            callIntent.data = Uri.parse("tel:" + "0 700 330 200")
+            startActivity(callIntent)
+        }
+
+        binding.emailLayout.setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.setDataAndType(Uri.parse("mailto:" + "dentistkg@gmail.com"), null)
+            startActivity(emailIntent)
+        }
+
+        binding.openMap.setOnClickListener {
+            val gmmIntentUri = Uri.parse("http://maps.google.com/maps?q=loc:"
+                    +40.5241845+","+72.8032641+" (" + "Dental clinic" + ")")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+
+
     }
 
-
-
 }
+
+
